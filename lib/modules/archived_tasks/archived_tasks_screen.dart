e45f31d9c32375fcd68e185d0a0107ea172381fb
ahossny61 <ahossny61@gmail.com>
2022-01-07 16:52:12 +0200

    Create ArchivedTasks Screen to project

---
+++ lib/modules/archived_tasks/archived_tasks_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class ArchiveTasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Archived Tasks ',
+        style: TextStyle(
+        fontSize: 25.0,
+        fontWeight: FontWeight.bold,
+      ),),
+    );
+  }
+
+}
