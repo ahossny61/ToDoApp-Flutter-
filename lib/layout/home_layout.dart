1b52db7e847cc2ec788cdf441e54b65302a5f21b
ahossny61 <ahossny61@gmail.com>
2022-01-07 16:39:51 +0200

    Finish BottomNavicationBar

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -2,10 +2,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeLayout extends StatelessWidget{
+class HomeLayout extends StatefulWidget{
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int currentIndex=0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToDO App'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child:Icon(
+          Icons.add,
+        ) ,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index){
+          setState(() {
+            currentIndex=index;
+          });
 
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon:Icon(Icons.menu),
+            label: 'Tasks'
+          ),
+          BottomNavigationBarItem(
+            icon:Icon(Icons.check_circle_outline),
+            label: 'Done',
+          ),
+          BottomNavigationBarItem(
+            icon:Icon(Icons.archive_outlined),
+            label: 'Archived',
+          )
+        ],
+      ),
+    );
+  }
 }
